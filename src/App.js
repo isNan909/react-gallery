c02d31dca5bdf9f685b14138576c1ff5507d6a04
ishan01 <ishanmanandhar@lftechnology.com>
2021-12-17 18:22:57 +0545

    simple react gallery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./data/images.json";
+import Modal from "./components/Modal";
 
 function App() {
+  const [clickedImg, setClickedImg] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(null);
+
+  const handleClick = (item, index) => {
+    setCurrentIndex(index);
+    setClickedImg(item.link);
+  };
+
+  const handelRotation = () => {
+    const totalLength = data.data.length;
+    if (currentIndex + 1 >= totalLength) {
+      setCurrentIndex(0);
+      const newUrl = data.data[0].link;
+      console.log(newUrl);
+      setClickedImg(newUrl);
+      return;
+    }
+    const newIndex = currentIndex + 1;
+    const newUrl = data.data.filter((item) => {
+      return data.data.indexOf(item) === newIndex;
+    });
+    const newItem = newUrl[0].link;
+    setClickedImg(newItem);
+    setCurrentIndex(newIndex);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      {data.data.map((item, index) => (
+        <div key={index} className="wrapper-images">
+          <img
+            src={item.link}
+            alt={item.text}
+            onClick={() => handleClick(item, index)}
+          />
+          <h2>{item.text}</h2>
+        </div>
+      ))}
+      <div>
+        {clickedImg && (
+          <Modal
+            clickedImg={clickedImg}
+            handelRotation={handelRotation}
+            setClickedImg={setClickedImg}
+          />
+        )}
+      </div>
     </div>
   );
 }
